1c36d1598135e7044c3202ab6ccdc6b6bbaf7730
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-03-05 17:26:48 +0500

    join room ui change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,46 +27,55 @@ export default function Home() {
 			</Head>
 			<main className="min-h-screen py-16  flex flex-col justify-center items-center">
 				<div className="max-w-xs mx-auto space-y-4 w-full">
-					<div>
-						<label
-							htmlFor="username"
-							className="block text-xs font-medium text-gray-700"
-						>
-							Username
-						</label>
-
-						<input
-							type="text"
-							value={user.name}
-							onChange={(e) =>
-								setUser({ ...user, name: e.target.value })
-							}
-							id="username"
-							className="mt-1 w-full rounded-xl border-gray-200 h-10 shadow-sm sm:text-sm"
-						/>
+					<div className="isolate -space-y-px rounded-xl shadow-sm">
+						<h1 className="text-center py-6 font-bold text-xl">
+							Join Room
+						</h1>
+						<div className="relative rounded-xl rounded-b-none px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-blue-600">
+							<label
+								htmlFor="name"
+								className="block text-xs font-medium text-gray-900"
+							>
+								Name
+							</label>
+							<input
+								type="text"
+								name="name"
+								value={user.name}
+								onChange={(e) =>
+									setUser({ ...user, name: e.target.value })
+								}
+								id="name"
+								className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+								placeholder="Jane Smith"
+							/>
+						</div>
+						<div className="relative rounded-xl rounded-t-none px-3 pt-2.5 pb-1.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-blue-600">
+							<label
+								htmlFor="room"
+								className="block text-xs font-medium text-gray-900"
+							>
+								Room
+							</label>
+							<input
+								type="text"
+								name="room"
+								value={room}
+								onChange={(e) => setRoom(e.target.value)}
+								id="room"
+								className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+								placeholder="123"
+							/>
+						</div>
 					</div>
-					<div>
-						<label
-							htmlFor="room"
-							className="block text-xs font-medium text-gray-700"
-						>
-							Room
-						</label>
 
-						<input
-							type="text"
-							value={room}
-							onChange={(e) => setRoom(e.target.value)}
-							id="room"
-							className="mt-1 w-full rounded-xl border-gray-200 h-10 shadow-sm sm:text-sm"
-						/>
-					</div>
 					<button
 						onClick={() => {
 							if (!user.name || !room) return;
 							router.push(`/${room}`);
 						}}
-						className="text-white bg-blue-600 h-10 text-sm rounded-xl w-full hover:bg-blue-700 transition-colors duration-300 ease-in-out"
+						type="button"
+						className="rounded-xl w-full bg-blue-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
 					>
 						Join
 					</button>
